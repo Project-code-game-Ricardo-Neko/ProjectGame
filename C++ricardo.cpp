8713f871e3533cad6a4a9c98e5e8c833e590a0f3
= <=duongquocanh368@gmail.com>
2022-10-14 12:30:37 +0700

    ez scam

--- C++ricardo.cpp
+++ C++ricardo.cpp
@@ -5,19 +5,29 @@ using namespace std;
 int main (){
     SetConsoleOutputCP(65001);
     system("cls"); Sleep(3000);
-    cout<<"Welcome to ricardo game\n"; Sleep(1000);
-    cout<<"Ricardo là dota\n";
+    cout<<"\tWelcome to ricardo game\n"; Sleep(1000);
+    cout<<"Ricardo là dota\n"; Sleep(1000);
     cout<<"get ready\n"; Sleep(2000); //khởi động
-       
-        
-        for (int j=10; j >= 0 ; j--){ //đếm 10 ->0 
+
+     int j = 10;     
+        for (int j = 10; j >= 0 ; j--){ //đếm 10 ->0 
             system("cls");
-    cout<<"Welcome to ricardo game\n"; //3 lệnh này để in giữ nguyên
+    cout<<"\tWelcome to ricardo game\n"; //3 lệnh này để in giữ nguyên
     cout<<"Ricardo là dota\n";
     cout<<"get ready\n"; 
             
             cout << j; Sleep(1000); //biến đếm
-        }
+        
+        
+        } Sleep(1000);
+       
+             system("cls");
+    cout<<"\tWelcome to ricardo game\n"; //3 lệnh này để in giữ nguyên
+    cout<<"Ricardo là dota\n";
+    cout<<"get ready\n"; 
+    cout<<"0 is gay dota scam ez\n";
+        Sleep(2000);
+
     while (TRUE){
         cout<<"DOTA DOTA "; //in =))
     }
