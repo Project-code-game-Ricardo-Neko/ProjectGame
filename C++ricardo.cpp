54109193809c23d41891e75ce956fceb87c690f6
= <=duongquocanh368@gmail.com>
2022-10-14 12:17:57 +0700

    chương trình đơn giản

--- C++ricardo.cpp
+++ C++ricardo.cpp
@@ -1,7 +1,26 @@
 #include<iostream>
+#include<windows.h>
 using namespace std;
 
 int main (){
-    cout<<"Thai_Gaming Super Gay Ricardo";
-    
+    SetConsoleOutputCP(65001);
+    system("cls"); Sleep(3000);
+    cout<<"Welcome to ricardo game\n"; Sleep(1000);
+    cout<<"Ricardo là dota\n";
+    cout<<"get ready\n"; Sleep(2000); //khởi động
+       
+        
+        for (int j=10; j >= 0 ; j--){ //đếm 10 ->0 
+            system("cls");
+    cout<<"Welcome to ricardo game\n"; //3 lệnh này để in giữ nguyên
+    cout<<"Ricardo là dota\n";
+    cout<<"get ready\n"; 
+            
+            cout << j; Sleep(1000); //biến đếm
+        }
+    while (TRUE){
+        cout<<"DOTA DOTA "; //in =))
+    }
+
+    return 0;
 }
